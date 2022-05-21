f7d0c8aad23597eeaf9d3f37d4bb8c6109dbc67f
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-05-21 16:00:54 -0400

    added server routes for post

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
 const Post = require("./Post");
+//=======================
+//Association between model and post
+User.hasMany(Post,{
+    foreignKey:'user_id'
+});
+//reverse association
+Post.belongsTo(User,{
+    foreignKey:'user_id'
+})
+
 module.exports = { User, Post };
